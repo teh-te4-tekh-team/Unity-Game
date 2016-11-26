97d70bccb9581e4b90295d6bac7eb730ac1c7b3a
iludart <peter.penev@hotmail.com>
2016-11-26 14:07:41 +0200

    connected player death with hud animation

---
+++ Teh-te4-teks-Z/Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverManager : MonoBehaviour {
+    public PlayerHealth playerHealth;
+
+    Animator animator;
+
+	// Use this for initialization
+	void Awake () {
+        animator = GetComponent<Animator>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerHealth.currentHealth <= 0)
+        {
+            animator.SetTrigger("PlayerDead");
+        }
+	}
+}
