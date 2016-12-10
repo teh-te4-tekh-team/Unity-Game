bff6be1aac554da799e56980a671d4d02a147fec
Gesh4o <georgi969696@abv.bg>
2016-12-10 15:19:25 +0200

    Add camera controller script. Reposition the player.

---
+++ Teh-te4-teks-Z/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject currentPlayer;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        this.offset = this.transform.position - this.currentPlayer.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = this.currentPlayer.transform.position + this.offset;
+        this.transform.LookAt(this.currentPlayer.transform);
+    }
+}
