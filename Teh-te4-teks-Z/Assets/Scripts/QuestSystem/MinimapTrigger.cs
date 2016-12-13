4960fa4eddef59720eab20b56725288b385506a2
Peter Penev <peter.penev@hotmail.com>
2016-12-13 04:19:07 +0200

    added minimap and a quest for aquiring it

---
+++ Teh-te4-teks-Z/Assets/Scripts/QuestSystem/MinimapTrigger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapTrigger : MonoBehaviour
+{
+
+    public QuestManager questManager;
+    public GameObject minimap;
+
+    bool alreadySet;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.questManager = FindObjectOfType<QuestManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Detector"))
+        {
+            return;
+        }
+
+        if (other.transform.root.CompareTag("Player"))
+        {
+            const int minimapQuestNumber = 2;
+            if (this.questManager.questCompleted[minimapQuestNumber] && !alreadySet)
+            {
+                this.minimap.SetActive(true);
+                this.alreadySet = true;
+            }
+        }
+    }
+}
